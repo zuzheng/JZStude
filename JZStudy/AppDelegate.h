7095dfb3ebedd35c335a36130c0fa990f487b895
Andre <pzz_078@163.com>
2017-10-23 14:23:31 +0800

    #项目初始

---
+++ JZStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JZStudy
+//
+//  Created by Andre on 2017/10/23.
+//  Copyright © 2017年 Andre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JZStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JZStudy
+//
+//  Created by Andre on 2017/10/23.
+//  Copyright © 2017年 Andre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
